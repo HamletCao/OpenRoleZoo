4cf40914d05ffdd0d931750a01506c0ed867c83c
likier <likier@sina.cn>
2017-07-31 15:41:40 +0800

    update test for csv

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,8 @@
 #include "orz/utils/format.h"
 #include "orz/utils/log.h"
 
+#include "orz/io/csv.h"
+
 #include <iostream>
 
 int main()
@@ -13,6 +15,19 @@ int main()
     orz::GlobalLogLevel(orz::INFO);
     RichLog(orz::STATUS) << "This " << "is " << "a " << "single " << "line.";
     RichLog(orz::INFO) << "This " << "is " << "another " << "single " << "line.";
+
+    std::vector<std::string> csv = {"1", "\"asdf\"", "asdf,asdf", "\"asdf\",\"asdf\""};
+    auto line = orz::CSVBuild(csv, ';');
+
+    std::cout << line << std::endl;
+
+    auto items = orz::CSVParse(line, ';');
+
+    for (auto item : items)
+    {
+        std::cout << item << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
