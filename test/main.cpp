2d554461d6430cd010b9184310da97d1955972eb
likier <likier@sina.cn>
2017-08-11 22:14:39 +0800

    update test/main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -6,20 +6,29 @@
 #include "orz/utils/log.h"
 
 #include "orz/io/csv.h"
+#include "orz/io/walker.h"
+#include "orz/io/dir.h"
+#include "orz/sync/canyon.h"
 
 #include <iostream>
+#include <memory>
 
 int main()
 {
     std::cout << orz::Format("Hey, I'm running!") << std::endl;
     orz::GlobalLogLevel(orz::INFO);
-    RichLog(orz::STATUS) << "This " << "is " << "a " << "single " << "line.";
-    RichLog(orz::INFO) << "This " << "is " << "another " << "single " << "line.";
+    ORZ_LOG(orz::STATUS) << "This " << "is " << "a " << "single " << "line.";
+    ORZ_LOG(orz::INFO) << "This " << "is " << "another " << "single " << "line.";
+
+    // ORZ_LOG(orz::FATAL) << "Must deal" << orz::crash;
 
     std::vector<std::string> csv = {"1", "\"asdf\"", "asdf,asdf", "\"asdf\",\"asdf\""};
     auto line = orz::CSVBuild(csv);
 
     std::cout << line << std::endl;
+    std::cout.flush();
+    std::cerr << line << std::endl;
+    std::cerr.flush();
 
     auto items = orz::CSVParse(line);
 
@@ -28,6 +37,17 @@ int main()
         std::cout << item << " ";
     }
     std::cout << std::endl;
+
+    auto files = orz::FindFilesRecursively("D:\\Working\\OpenRoleZoo\\OpenRoleZoo\\lib");
+    auto dirs = orz::FindFlodersRecursively("D:\\Working\\OpenRoleZoo\\OpenRoleZoo\\lib");
+
+    using namespace orz;
+    std::cout << files << std::endl;
+    std::cout << dirs << std::endl;
+
+    orz::Canyon<int> a([](int i) {std::cout << i << std::endl;});
+    a(10);
+    a(20);
 
     return 0;
 }
