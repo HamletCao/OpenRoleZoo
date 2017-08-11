0383ee4d6d7811557f8df8feda197c1de08e8dff
likier <likier@sina.cn>
2017-08-11 22:14:28 +0800

    add Canyon

---
+++ include/orz/sync/canyon.h
@@ -0,0 +1,72 @@
+//
+// Created by Lby on 2017/8/11.
+//
+
+#ifndef ORZ_SYNC_CANYON_H
+#define ORZ_SYNC_CANYON_H
+
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <thread>
+#include <queue>
+
+namespace orz {
+
+    template<typename T>
+    class Canyon {
+    public:
+        using Operation = std::function<void(const T &)>;
+
+        template<typename OP>
+        explicit Canyon(OP op, int size = -1)
+                : _work(true), _core(&Canyon::operating, this), _size(size) {
+            this->_op = [op](const T &arg) { op(arg); };
+        }
+
+        ~Canyon() {
+            this->join();
+            _work = false;
+            _cond.notify_all();
+            _core.join();
+        }
+
+        void operator()(const T &arg) {
+            std::unique_lock<std::mutex> _locker(_mutex);
+            while (_size > 0 && _task.size() >= _size) _cond.wait(_locker);
+            _task.push(arg);
+            _cond.notify_all();
+        }
+
+        void join() {
+            std::unique_lock<std::mutex> _locker(_mutex);
+            while (_task.size()) _cond.wait(_locker);
+        }
+
+    private:
+        void operating() {
+            std::unique_lock<std::mutex> _locker(_mutex);
+            while (_work) {
+                while (_work && _task.size() == 0) _cond.wait(_locker);
+                if (!_work) break;
+                T arg = _task.front();
+                _task.pop();
+                _op(arg);
+                _cond.notify_all();
+            }
+        }
+
+        Operation _op;
+        std::queue<T> _task;
+        std::thread _core;
+        std::mutex _mutex;
+        std::condition_variable _cond;
+        std::atomic<bool> _work;
+        int _size;
+    };
+
+}
+
+
+#endif //ORZ_SYNC_CANYON_H
---
+++ src/orz/sync/canyon.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Lby on 2017/8/11.
+//
+
+#include "orz/sync/canyon.h"
+
+namespace orz {
+
+}
