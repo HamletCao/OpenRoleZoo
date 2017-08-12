37ac2e20a88bf015866fcb460266403871b9826a
likier <likier@sina.cn>
2017-08-12 18:22:38 +0800

    ready to add a memory pot

---
+++ include/orz/mem/pot.h
@@ -0,0 +1,15 @@
+//
+// Created by Lby on 2017/8/12.
+//
+
+#ifndef ORZ_MEM_POOL_H
+#define ORZ_MEM_POOL_H
+
+namespace orz {
+
+    class MemoryPot {
+        
+    };
+}
+
+#endif //ORZ_MEM_POOL_H
---
+++ src/orz/mem/pot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lby on 2017/8/12.
+//
+
+#include "orz/mem/pot.h"
