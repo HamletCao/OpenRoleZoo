0c04321467d24edc333bc49d6145504046471b6a
likier <likier@sina.cn>
2017-08-14 21:46:33 +0800

    set mem/pot API

--- include/orz/mem/pot.h
+++ include/orz/mem/pot.h
@@ -5,10 +5,30 @@
 #ifndef ORZ_MEM_POOL_H
 #define ORZ_MEM_POOL_H
 
+#include <mutex>
+#include <memory>
+
 namespace orz {
 
     class MemoryPot {
-        
+    public:
+        void *alloc(size_t _size);
+        const void *alloc(size_t _size) const;
+
+        template <typename T>
+        T *alloc(size_t _count) {
+            return reinterpret_cast<T*>(this->alloc(sizeof(T) * _count));
+        }
+
+        template <typename T>
+        const T *alloc(size_t _count) const {
+            return reinterpret_cast<T*>(this->alloc(sizeof(T) * _count));
+        }
+
+        void free() const;
+    private:
+        size_t _capacity;
+        std::shared_ptr<void> _data;
     };
 }
 
