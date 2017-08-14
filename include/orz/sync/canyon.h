9ecfc090199783465ff5ae79ee547445a5f26279
likier <likier@sina.cn>
2017-08-14 21:45:10 +0800

    update canyon to V2, more general

--- include/orz/sync/canyon.h
+++ include/orz/sync/canyon.h
@@ -11,59 +11,38 @@
 #include <atomic>
 #include <thread>
 #include <queue>
+#include <future>
 
 namespace orz {
 
-    template<typename T>
     class Canyon {
     public:
-        using Operation = std::function<void(const T &)>;
+        using Operation = std::function<void()>;
 
-        template<typename OP>
-        explicit Canyon(OP op, int size = -1)
-                : _work(true), _size(size) {
-            this->_op = [op](const T &arg) { op(arg); };
-            this->_core = std::thread(&Canyon::operating, this);
-        }
+        explicit Canyon(int size = -1);
 
-        ~Canyon() {
-            this->join();
-            _work = false;
-            _cond.notify_all();
-            _core.join();
-        }
+        ~Canyon();
 
-        void operator()(const T &arg) const {
-            std::unique_lock<std::mutex> _locker(_mutex);
-            while (_size > 0 && _task.size() >= static_cast<size_t >(_size)) _cond.wait(_locker);
-            _task.push(arg);
-            _cond.notify_all();
-        }
+        template<typename FUNC, typename... Args>
+        void operator()(FUNC func, Args... args) const {
+            auto op = [func, args...]() {
+                func(args...);
+            };
+            this->push(op);
+        };
 
-        void join() const {
-            std::unique_lock<std::mutex> _locker(_mutex);
-            while (_task.size()) _cond.wait(_locker);
-        }
+        void join() const;
 
     private:
         Canyon(const Canyon &that) = delete;
 
         const Canyon &operator=(const Canyon &that) = delete;
 
-        void operating() const {
-            std::unique_lock<std::mutex> _locker(_mutex);
-            while (_work) {
-                while (_work && _task.size() == 0) _cond.wait(_locker);
-                if (!_work) break;
-                T arg = _task.front();
-                _task.pop();
-                _op(arg);
-                _cond.notify_all();
-            }
-        }
+        void push(const Operation &op) const;
 
-        Operation _op;
-        mutable std::queue<T> _task;
+        void operating() const;
+
+        mutable std::queue<Operation> _task;
         mutable std::mutex _mutex;
         mutable std::condition_variable _cond;
         std::atomic<bool> _work;
--- src/orz/sync/canyon.cpp
+++ src/orz/sync/canyon.cpp
@@ -6,4 +6,39 @@
 
 namespace orz {
 
+    Canyon::Canyon(int size)
+            : _work(true), _size(size) {
+        this->_core = std::thread(&Canyon::operating, this);
+    }
+
+    Canyon::~Canyon() {
+        this->join();
+        _work = false;
+        _cond.notify_all();
+        _core.join();
+    }
+
+    void Canyon::join() const {
+        std::unique_lock<std::mutex> _locker(_mutex);
+        while (_task.size()) _cond.wait(_locker);
+    }
+
+    void Canyon::push(const Operation &op) const {
+        std::unique_lock<std::mutex> _locker(_mutex);
+        while (_size > 0 && _task.size() >= static_cast<size_t >(_size)) _cond.wait(_locker);
+        _task.push(op);
+        _cond.notify_all();
+    }
+
+    void Canyon::operating() const {
+        std::unique_lock<std::mutex> _locker(_mutex);
+        while (_work) {
+            while (_work && _task.size() == 0) _cond.wait(_locker);
+            if (!_work) break;
+            auto func = _task.front();
+            _task.pop();
+            func();
+            _cond.notify_all();
+        }
+    }
 }
