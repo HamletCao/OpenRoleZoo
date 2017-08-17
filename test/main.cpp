1f6cdec1045e37bbc5fdb235e164e453e589ba8a
likier <likier@sina.cn>
2017-08-17 21:09:10 +0800

    update test/main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -9,21 +9,10 @@
 #include "orz/io/walker.h"
 #include "orz/io/dir.h"
 #include "orz/sync/canyon.h"
+#include "orz/io/jug/piece.h"
 
 #include <iostream>
-#include <memory>
-
-#include <future>
-
-int get_int()
-{
-    return 233;
-}
-
-void print(const std::string &str)
-{
-    std::cout << str << std::endl;
-}
+#include <fstream>
 
 int main()
 {
@@ -57,13 +46,33 @@ int main()
     std::cout << files << std::endl;
     std::cout << dirs << std::endl;
 
-    auto func = [](int i) {std::cout << i << std::endl;};
-    const orz::Canyon a;
-    a(func, 10);
-    a(func, 20);
+    std::string jug_name = "test.jug";
 
-    a(get_int);
-    a(print, "a");
+    std::ofstream outfile(jug_name, std::ios::binary);
+
+    orz::DictPiece dp;
+
+
+    dp["a"] = std::make_shared<StringPiece>("asdf");
+    dp["b"] = std::make_shared<IntPiece>(10);
+
+    orz::Piece::Write(outfile, dp);
+
+    outfile.close();
+
+    std::ifstream infile(jug_name, std::ios::binary);
+
+    auto dp2_r = orz::Piece::Read(infile);
+    std::cout << dp2_r->type() << std::endl;
+    auto dp2 = reinterpret_cast<DictPiece *>(dp2_r.get());
+
+    auto dp2_0 = (*dp2)["a"];
+    std::cout << reinterpret_cast<StringPiece *>(dp2_0.get())->get() << std::endl;
+
+    auto dp2_1 = (*dp2)["b"];
+    std::cout << reinterpret_cast<IntPiece *>(dp2_1.get())->get() << std::endl;
+
+    infile.close();
 
     return 0;
 }
