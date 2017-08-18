0999f32ebb1cdd26147502e3c7acac702c9a5756
likier <likier@sina.cn>
2017-08-18 15:20:07 +0800

    add jug interface for piece

--- include/orz/io/jug/jug.h
+++ include/orz/io/jug/jug.h
@@ -9,27 +9,142 @@
 
 namespace orz {
 
-    std::shared_ptr<Piece> jug_parse(const std::string &buffer);
+    /*
+     * @brief interface of piece
+     */
+    class jug {
+    public:
 
-    std::string jug_build(const std::shared_ptr<Piece> &pie);
+        jug();
 
-    void jug_build(std::string &buffer, const std::shared_ptr<Piece> &pie);
+        jug(Piece::Type type);
 
-    std::string jug_build(const Piece &pie);
+        jug(std::nullptr_t);
 
-    void jug_build(std::string &buffer, Piece &pie);
+        jug(int val);
 
-    std::shared_ptr<Piece> jug_read(const std::string &filename);
+        jug(unsigned int val) : jug(static_cast<int>(val)) {}
 
-    std::shared_ptr<Piece> jug_read(std::istream &in);
+        jug(char val) : jug(static_cast<int>(val)) {}
 
-    void write(std::ostream &out, const std::shared_ptr<Piece> &pie);
+        jug(unsigned char val) : jug(static_cast<int>(val)) {}
 
-    void write(std::ostream &out, const Piece &pie);
+        jug(long val) : jug(static_cast<int>(val)) {}
 
-    void write(const std::string &filename, const std::shared_ptr<Piece> &pie);
+        jug(unsigned long val) : jug(static_cast<int>(val)) {}
 
-    void write(const std::string &filename, const Piece &pie);
+        jug(long long val) : jug(static_cast<int>(val)) {}
+
+        jug(unsigned long long val) : jug(static_cast<int>(val)) {}
+
+        jug(float val);
+
+        jug(double val) : jug(static_cast<float>(val)) {}
+
+        jug(const std::string &val);
+
+        template<size_t _size>
+        jug(const char (&val)[_size]) : jug(std::string(val)) {}
+
+        jug(const char *val) : jug(std::string(val)) {}
+
+        bool valid(Piece::Type type);
+
+        // common type set function
+
+        // common type function
+        operator bool() const;
+
+        operator int() const;
+
+        operator float() const;
+
+        operator std::string() const;
+
+        // string, binary, list, dict function
+        size_t size() const;
+
+        // list function
+        jug index(size_t i);
+
+        jug operator[](size_t i) {
+            return index(i);
+        }
+
+        jug index(size_t i, const jug &value);
+
+        jug &append(const jug &value);
+
+        // dict function
+        jug index(const std::string &key);
+
+        jug operator[](const std::string &key) {
+            return index(key);
+        }
+
+        jug index(const std::string &key, const jug &value);
+
+        std::vector<std::string> keys() const;
+
+        // binary function
+        jug &set_bits(void *buffer, size_t size);
+
+        jug &push_bits(void *buffer, size_t size);
+
+        template<typename T>
+        jug &push_bits(const T &val) {
+            switch (m_pie->type()) {
+                case Piece::BINARY: {
+                    auto buff = reinterpret_cast<BinaryPiece *>(m_pie.get());
+                    buff->push_bits<T>(val);
+                    return *this;
+                }
+                default:
+                    throw Exception("This jug has no method push_bits<T>(val)");
+            }
+        }
+
+    private:
+        jug(const std::shared_ptr<Piece> &pie)
+                : m_pie(pie) {}
+
+        operator std::shared_ptr<Piece>() const {
+            return m_pie;
+        }
+
+        std::shared_ptr<Piece> m_pie;
+
+    public:
+
+        friend std::ostream &operator<<(std::ostream &out, const jug &e);
+
+        friend jug jug_parse(const std::string &buffer);
+
+        friend std::string jug_build(const jug &j);
+
+        friend jug jug_read(const std::string &filename);
+
+        friend jug jug_read(std::istream &in);
+
+        friend bool jug_write(const std::string &filename, const jug &j);
+
+        friend void jug_write(std::ostream &out, const jug &j);
+
+    };
+
+    std::ostream &operator<<(std::ostream &out, const jug &e);
+
+    jug jug_parse(const std::string &buffer);
+
+    std::string jug_build(const jug &j);
+
+    jug jug_read(const std::string &filename);
+
+    jug jug_read(std::istream &in);
+
+    bool jug_write(const std::string &filename, const jug &j);
+
+    void jug_write(std::ostream &out, const jug &j);
 }
 
 
--- src/orz/io/jug/jug.cpp
+++ src/orz/io/jug/jug.cpp
@@ -3,3 +3,288 @@
 //
 
 #include "orz/io/jug/jug.h"
+
+#include <fstream>
+
+namespace orz {
+
+    jug::jug()
+            : jug(Piece::NIL) {}
+
+    jug::jug(Piece::Type type)
+            : m_pie(Piece::Get(type)) {}
+
+    jug::jug(std::nullptr_t)
+            : m_pie(std::make_shared<NilPiece>()) {}
+
+    jug::jug(int val)
+            : m_pie(std::make_shared<IntPiece>(val)) {}
+
+    jug::jug(float val)
+            : m_pie(std::make_shared<FloatPiece>(val)) {}
+
+    jug::jug(const std::string &val)
+            : m_pie(std::make_shared<StringPiece>(val)) {}
+
+    bool jug::valid(Piece::Type type) {
+        return m_pie && m_pie->type() == type;
+    }
+
+    jug::operator bool() const {
+        return m_pie && m_pie->notnil();
+    }
+
+    jug::operator int() const {
+        switch (m_pie->type()) {
+            case Piece::INT:
+                return reinterpret_cast<IntPiece *>(m_pie.get())->get();
+            case Piece::FLOAT:
+                return static_cast<int>(
+                        reinterpret_cast<FloatPiece *>(m_pie.get())->get());
+            case Piece::STRING:
+                return std::atoi(
+                        reinterpret_cast<StringPiece *>(m_pie.get())->get().c_str());
+            default:
+                throw Exception("Can not convert this jug to int");
+        }
+    }
+
+    jug::operator float() const {
+        switch (m_pie->type()) {
+            case Piece::INT:
+                return static_cast<float>(
+                        reinterpret_cast<IntPiece *>(m_pie.get())->get());
+            case Piece::FLOAT:
+                return reinterpret_cast<FloatPiece *>(m_pie.get())->get();
+            case Piece::STRING:
+                return static_cast<float>(
+                        std::atof(reinterpret_cast<StringPiece *>(m_pie.get())->get().c_str()));
+            default:
+                throw Exception("Can not convert this jug to float");
+        }
+    }
+
+    jug::operator std::string() const {
+        switch (m_pie->type()) {
+            case Piece::STRING:
+                return reinterpret_cast<StringPiece *>(m_pie.get())->get();
+            default:
+                throw Exception("Can not convert this jug to string");
+        }
+    }
+
+    // string, binary, list, dict function
+    size_t jug::size() const {
+        switch (m_pie->type()) {
+            case Piece::STRING:
+                return reinterpret_cast<StringPiece *>(m_pie.get())->get().length();
+            case Piece::BINARY:
+                return reinterpret_cast<BinaryPiece *>(m_pie.get())->size();
+            case Piece::LIST:
+                return reinterpret_cast<ListPiece *>(m_pie.get())->size();
+            case Piece::DICT:
+                return reinterpret_cast<DictPiece *>(m_pie.get())->size();
+            default:
+                throw Exception("This jug has no method size()");
+        }
+    }
+
+    // list function
+    jug jug::index(size_t i) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::LIST);
+            case Piece::LIST: {
+                auto list = reinterpret_cast<ListPiece *>(m_pie.get());
+                if (i < 0 || i >= list->size()) {
+                    return jug();
+                } else {
+                    return list->index(i);
+                }
+            }
+            default:
+                throw Exception("This jug has no method index(i)");
+        }
+    }
+
+    jug jug::index(size_t i, const jug &value) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::LIST);
+            case Piece::LIST: {
+                auto list = reinterpret_cast<ListPiece *>(m_pie.get());
+                if (i < 0 || i < list->size()) {
+                    throw Exception("Index out of range");
+                } else {
+                    return list->index(i, value.m_pie);
+                }
+            }
+            default:
+                throw Exception("This jug has no method index(i, value)");
+        }
+    }
+
+    jug &jug::append(const jug &value) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::LIST);
+            case Piece::LIST: {
+                auto list = reinterpret_cast<ListPiece *>(m_pie.get());
+                list->push_back(value.m_pie);
+                return *this;
+            }
+            default:
+                throw Exception("This jug has no method index(i, value)");
+        }
+    }
+
+    // dict function
+    jug jug::index(const std::string &key) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::DICT);
+            case Piece::DICT: {
+                auto dict = reinterpret_cast<DictPiece *>(m_pie.get());
+                if (dict->has_key(key)) {
+                    return dict->index(key);
+                } else {
+                    return jug();
+                }
+            }
+            default:
+                throw Exception("This jug has no method index(key)");
+        }
+    }
+
+    jug jug::index(const std::string &key, const jug &value) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::DICT);
+            case Piece::DICT: {
+                auto dict = reinterpret_cast<DictPiece *>(m_pie.get());
+                return dict->index(key, value.m_pie);
+            }
+            default:
+                throw Exception("This jug has no method index(key, value)");
+        }
+    }
+
+    std::vector<std::string> jug::keys() const {
+        switch (m_pie->type()) {
+            case Piece::DICT: {
+                auto dict = reinterpret_cast<DictPiece *>(m_pie.get());
+                return dict->keys();
+            }
+            default:
+                throw Exception("This jug has no method keys()");
+        }
+    }
+
+    // binary function
+    jug &jug::set_bits(void *buffer, size_t size) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::BINARY);
+            case Piece::BINARY: {
+                auto buff = reinterpret_cast<BinaryPiece *>(m_pie.get());
+                buff->set_bits(buffer, size);
+                return *this;
+            }
+            default:
+                throw Exception("This jug has no method set_bits(buffer, size)");
+        }
+    }
+
+    jug &jug::push_bits(void *buffer, size_t size) {
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                m_pie = Piece::Get(Piece::BINARY);
+            case Piece::BINARY: {
+                auto buff = reinterpret_cast<BinaryPiece *>(m_pie.get());
+                buff->push_bits(buffer, size);
+                return *this;
+            }
+            default:
+                throw Exception("This jug has no method set_bits(buffer, size)");
+        }
+    }
+
+    std::ostream &operator<<(std::ostream &out, const jug &e) {
+        auto &m_pie = e.m_pie;
+        switch (m_pie->type()) {
+            case Piece::NIL:
+                return out << "nil";
+            case Piece::INT:
+                return out << reinterpret_cast<IntPiece *>(m_pie.get())->get();
+            case Piece::FLOAT:
+                return out << reinterpret_cast<FloatPiece *>(m_pie.get())->get();
+            case Piece::STRING:
+                return out << '\"' << reinterpret_cast<StringPiece *>(m_pie.get())->get() << '\"';
+            case Piece::BINARY:
+                return out << "binary:" << reinterpret_cast<BinaryPiece *>(m_pie.get())->size();
+            case Piece::LIST: {
+                auto list = reinterpret_cast<ListPiece *>(m_pie.get());
+                out << '[';
+                for (size_t i = 0; i < list->size(); ++i) {
+                    if (i) out << ',' << ' ';
+                    out << jug(list->index(i));
+                }
+                out << ']';
+                return out;
+            }
+            case Piece::DICT: {
+                auto dict = reinterpret_cast<DictPiece *>(m_pie.get());
+                out << '{';
+                bool first = true;
+                for (auto &key : dict->keys()) {
+                    if (!first) out << ',' << ' ';
+                    else first = false;
+                    out << key << ':' << jug(dict->index(key));
+                }
+                out << '}';
+                return out;
+            }
+        }
+
+        return out;
+    }
+
+
+    jug jug_parse(const std::string &buffer) {
+        std::istringstream iss(buffer, std::ios::binary);
+        return Piece::Read(iss);
+    }
+
+    std::string jug_build(const jug &j) {
+        std::ostringstream oss(std::ios::binary);
+        Piece::Write(oss, j.m_pie);
+        return oss.str();
+    }
+
+    jug jug_read(const std::string &filename) {
+        std::ifstream infile(filename, std::ios::binary);
+        if (infile.is_open()) {
+            return Piece::Read(infile);
+        } else {
+            return jug();
+        }
+    }
+
+    jug jug_read(std::istream &in) {
+        return Piece::Read(in);
+    }
+
+    bool jug_write(const std::string &filename, const jug &j) {
+        std::ofstream outfile(filename, std::ios::binary);
+        if (outfile.is_open()) {
+            Piece::Write(outfile, j.m_pie);
+            return true;
+        }
+        return false;
+    }
+
+    void jug_write(std::ostream &out, const jug &j) {
+        Piece::Write(out, j.m_pie);
+    }
+
+}
