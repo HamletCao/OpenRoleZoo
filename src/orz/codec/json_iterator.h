8d8f14bd245b93fbddc4aa2b421af3ca939d2b01
likier <likier@sina.cn>
2018-01-10 23:29:57 +0800

    update json_iterator

--- src/orz/codec/json_iterator.h
+++ src/orz/codec/json_iterator.h
@@ -5,6 +5,8 @@
 #ifndef ORZ_CODEC_JSON_ITERATOR_H
 #define ORZ_CODEC_JSON_ITERATOR_H
 
+#include "orz/utils/log.h"
+
 namespace orz {
 
     class json_iterator {
@@ -16,25 +18,67 @@ namespace orz {
         }
 
         const char &operator*() const {
-            if (index < 0 || index >= size) return null_char;
+            if (index < 0 || index >= size) ORZ_LOG(ERROR) << "index out of range" << crash;
             return data[index];
         }
 
-        json_iterator operator++() {
-            return json_iterator(data, size, ++index);
+        const json_iterator begin() const {
+            return json_iterator(data, size, 0);
+        }
+
+        const json_iterator end() const {
+            return json_iterator(data, size, size);
+        }
+
+        json_iterator &operator++() {
+            ++index;
+            return *this;
         }
 
         const json_iterator operator++(int) {
             return json_iterator(data, size, index++);
         }
 
+        json_iterator &operator+=(int shift) {
+            index += shift;
+            return *this;
+        }
+
+        json_iterator &operator-=(int shift) {
+            index -= shift;
+            return *this;
+        }
+
+        bool operator==(const json_iterator &it)const {
+            return self::data == it.data && self::size == it.size && self::index == it.index;
+        }
+
+        bool operator!=(const json_iterator &it)const {
+            return !self::operator==(it);
+        }
+
+        friend const json_iterator operator+(const json_iterator &it, int shift) {
+            return json_iterator(it.data, it.size, it.index + shift);
+        }
+
+        friend const json_iterator operator+(int shift, const json_iterator &it) {
+            return json_iterator(it.data, it.size, it.index + shift);
+        }
+
+        friend const json_iterator operator-(const json_iterator &it, int shift) {
+            return json_iterator(it.data, it.size, it.index - shift);
+        }
+
+        friend int operator-(const json_iterator &lhs, const json_iterator &rhs) {
+            if (lhs.data != rhs.data) ORZ_LOG(ERROR) << "can not sub iterators from different init" << crash;
+            return lhs.index - rhs.index;
+        }
+
     private:
         const char *data;
         int size;
         int index;
-        static const char null_char = '\0';
     };
-
 }
 
 #endif //ORZ_CODEC_JSON_ITERATOR_H
