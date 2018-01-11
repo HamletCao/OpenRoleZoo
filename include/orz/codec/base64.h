cf330d8bbd4000782dab86261ba93dc960fa739f
likier <likier@sina.cn>
2018-01-11 11:13:34 +0800

    base add base64 encode and decode

--- include/orz/codec/base64.h
+++ include/orz/codec/base64.h
@@ -5,10 +5,11 @@
 #ifndef ORZ_CODEC_BASE64_H
 #define ORZ_CODEC_BASE64_H
 
+#include <string>
 
-class base64 {
-
-};
-
+namespace orz {
+    std::string base64_encode(const std::string &str);
+    std::string base64_decode(const std::string &str);
+}
 
 #endif //ORZ_CODEC_BASE64_H
--- src/orz/codec/base64.cpp
+++ src/orz/codec/base64.cpp
@@ -2,4 +2,16 @@
 // Created by lby on 2018/1/10.
 //
 
-#include "../../../include/orz/codec/base64.h"
+#include "orz/codec/base64.h"
+
+
+
+namespace orz {
+    std::string base64_encode(const std::string &str) {
+        return str;
+    }
+
+    std::string base64_decode(const std::string &str) {
+        return str;
+    }
+}
