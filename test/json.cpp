546e4aa5305216d50841f4e215a4f9fc687f800a
likier <likier@sina.cn>
2018-01-15 21:31:32 +0800

    update json tester

--- test/json.cpp
+++ test/json.cpp
@@ -5,9 +5,28 @@
 #include <orz/codec/json.h>
 #include <orz/utils/log.h>
 
-int main(int argc, char *argv[])
-{
-    std::string json_string = R"({"version":"5.1.2","date":"@date","name":"VIPLFaceDetector5.1.2.m9d6.640x480.dat","data":"@file@model/VIPLFaceDetector5.1.2.m9d6.640x480.dat","params":{"stride":4,"end2end":1,"thresh1":0.7,"thresh2":0.7,"thresh3":0.85},"input":{"width_limit":640,"height_limit":480}})";
+int main(int argc, char *argv[]) {
+    std::string json_string =
+            R"({
+    "name": "json2sta",
+    "date": "@date",
+    "stride": 4,
+    "scale": 1.414,
+    "threshold": [
+        0.7,
+        0.7,
+        0.85
+    ],
+    "transform": {
+        "type": "sigmoid",
+        "param": [
+            1,
+            2.3
+        ]
+    },
+    "end2end" : false,
+    "data" : "@file@1.jpg"
+})";
     auto value = orz::json2jug(json_string);
     ORZ_LOG(orz::INFO) << "Original json: " << json_string;
     ORZ_LOG(orz::INFO) << "Converted jug: " << value;
