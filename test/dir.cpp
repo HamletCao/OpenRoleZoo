12a254be822915307ebc811c71e4687aed052961
likier <likier@sina.cn>
2018-02-09 20:06:25 +0800

    update test dir

--- test/dir.cpp
+++ test/dir.cpp
@@ -11,17 +11,17 @@ int main(int argc, char *argv[]) {
     auto result = orz::copy("1.jpg", "3.jpg");
     ORZ_LOG(orz::INFO) << "Copy 1.jpg to 3.jpg, status: " << std::boolalpha << result;
 
-    ORZ_LOG(orz::INFO) << "Current work dir: " << orz::getcwd();
-    ORZ_LOG(orz::INFO) << "Self module name: " << orz::getself();
-    ORZ_LOG(orz::INFO) << "Current exe name: " << orz::getexed();
+    ORZ_LOG(orz::INFO) << "Current work dir: \"" << orz::getcwd() << "\"";
+    ORZ_LOG(orz::INFO) << "Self module name: \"" << orz::getself() << "\"";
+    ORZ_LOG(orz::INFO) << "Current exe name: \"" << orz::getexed() << "\"";
 
     ORZ_LOG(orz::INFO) << "Change work dir to ..";
 
     orz::cd("..");
 
-    ORZ_LOG(orz::INFO) << "Current work dir: " << orz::getcwd();
-    ORZ_LOG(orz::INFO) << "Self module name: " << orz::getself();
-    ORZ_LOG(orz::INFO) << "Current exe name: " << orz::getexed();
+    ORZ_LOG(orz::INFO) << "Current work dir: \"" << orz::getcwd() << "\"";
+    ORZ_LOG(orz::INFO) << "Self module name: \"" << orz::getself() << "\"";
+    ORZ_LOG(orz::INFO) << "Current exe name: \"" << orz::getexed() << "\"";
 
 
 
