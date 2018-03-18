53881ef8d1eed4a25e48f4d5bc24ffc16bc13b03
likier <likier@sina.cn>
2018-03-18 11:09:43 +0800

    add windows cmake install

---
+++ craft/build.win.vc12.x64.debug.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+set "BUILD_DIR=build.win.vc12.x64.debug"
+set "BUILD_TYPE=Debug"
+set "PLATFORM=amd64"
+set "PLATFORM_TARGET=x64"
+
+set "WITH_SSL=ON"
+set "SSL_HOME=D:\3rd\OpenSSL64"
+
+set "INSTALL_DIR=D:\3rd\local\"
+
+call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" %PLATFORM%
+
+cd %~dp0
+
+md "%BUILD_DIR%"
+
+cd "%BUILD_DIR%"
+
+md "%INSTALL_DIR%"
+
+cmake "%~dp0.." ^
+-G"NMake Makefiles JOM" ^
+-DCMAKE_BUILD_TYPE="%BUILD_TYPE%" ^
+-DPLATFORM="%PLATFORM_TARGET%" ^
+-DORZ_WITH_OPENSSL="%WITH_SSL%" ^
+-DOPENSSL_ROOT_DIR="%SSL_HOME%" ^
+-DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"
+
+jom -j16 install
---
+++ craft/build.win.vc12.x86.debug.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+set "BUILD_DIR=build.win.vc12.x86.debug"
+set "BUILD_TYPE=Debug"
+set "PLATFORM=x86"
+set "PLATFORM_TARGET=x86"
+
+set "WITH_SSL=ON"
+set "SSL_HOME=D:\3rd\OpenSSL32"
+
+set "INSTALL_DIR=D:\3rd\local\"
+
+call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" %PLATFORM%
+
+cd %~dp0
+
+md "%BUILD_DIR%"
+
+cd "%BUILD_DIR%"
+
+md "%INSTALL_DIR%"
+
+cmake "%~dp0.." ^
+-G"NMake Makefiles JOM" ^
+-DCMAKE_BUILD_TYPE="%BUILD_TYPE%" ^
+-DPLATFORM="%PLATFORM_TARGET%" ^
+-DORZ_WITH_OPENSSL="%WITH_SSL%" ^
+-DOPENSSL_ROOT_DIR="%SSL_HOME%" ^
+-DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"
+
+jom -j16 install
