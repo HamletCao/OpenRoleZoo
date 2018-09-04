ad3e1d14568357a3ae1118c09bf0b0dc4632ca1c
seetadev <seetadev@shelf.local>
2018-09-05 01:11:14 +0800

    update cmake.sh to trans parameters

--- cmake.sh
+++ cmake.sh
@@ -2,6 +2,9 @@
 
 BASE=$(cd `dirname $0`; pwd)
 
-cmake "$BASE" -DCMAKE_TOOLCHAIN_FILE="$BASE"/iOS.cmake -DORZ_WITH_OPENSSL=ON -DOPENSSL_INCLUDE_DIR="$BASE"/../OpenSSL-for-iPhone/include
+cmake "$BASE" -DCMAKE_TOOLCHAIN_FILE="$BASE"/iOS.cmake $@ \
+-DORZ_WITH_OPENSSL=ON \
+-DOPENSSL_INCLUDE_DIR="$BASE"/../OpenSSL-for-iPhone/include
+
 # -DIOS_PLATFOMR=OS
 # -DIOS_PLATFORM=SIMULATOR64
