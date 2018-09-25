b548975dad4b28563eafa5529b938a3279849300
kaizhou.li <kaizhou.li@seetatech.com>
2018-09-26 02:45:06 +0800

    base add hash table's insert and find method

--- include/orz/tools/resources.h
+++ include/orz/tools/resources.h
@@ -17,15 +17,6 @@
 namespace orz {
 
     namespace resources {
-        struct orz_resources {
-            const char *data;
-            size_t size;
-        };
-
-        const struct orz_resources orz_resources_get(const char *url);
-
-        void orz_resources_list();
-
         static const char *const code_header[] = {
                 "#ifndef _INC_ORZ_RESOURCES_AUTO_COMPILATION_H",
                 "#define _INC_ORZ_RESOURCES_AUTO_COMPILATION_H",
@@ -41,7 +32,7 @@ namespace orz {
                 "    size_t size;",
                 "};",
                 "",
-                "const struct orz_resources orz_resources_get(const char *url);",
+                "const struct orz_resources orz_resources_get(const char *key);",
                 // "void orz_resources_list();",
                 "",
                 "#ifdef __cplusplus",
@@ -50,6 +41,175 @@ namespace orz {
                 "",
                 "#endif //_INC_ORZ_RESOURCES_AUTO_COMPILATION_H",
                 "",
+        };
+
+        static inline unsigned int ELFhash(const char *str) {
+            unsigned int hash = 0;
+            unsigned int x = 0;
+            while (*str) {
+                hash = (hash << 4) + *str;
+                if ((x = hash & 0xf0000000) != 0) {
+                    hash ^= (x >> 24);
+                    hash &= ~x;
+                }
+                str++;
+            }
+            return (hash & 0x7fffffff);
+        }
+
+        class resources_hash_node {
+        public:
+            using hash_type = unsigned int;
+
+            resources_hash_node() = default;
+            explicit resources_hash_node(const std::string &key, ssize_t next = -1)
+                    : key(key), hash(ELFhash(key.c_str())), next(next) {}
+
+            std::string key;
+            hash_type hash;
+            ssize_t next = -1;
+        };
+
+        class resources_has_table {
+        public:
+            using self = resources_has_table;
+
+            using hash_type = unsigned int;
+            using index_type = hash_type;
+
+            explicit resources_has_table(unsigned int size) : m_nodes(size_t(size > 2 ? size : 2), nullptr) {}
+
+            resources_has_table() : resources_has_table(2) {}
+
+            ~resources_has_table() {
+                for (auto node : m_nodes) {
+                    delete node;
+                }
+            }
+
+            resources_has_table(const self &) = delete;
+            self &operator==(const self &) = delete;
+
+            resources_hash_node *insert(const std::string &key) {
+                auto *found_node = this->find(key);
+                if (found_node) {
+                    // TODO: set value
+                    return found_node;
+                }
+                auto *new_node = new resources_hash_node(key);
+                insert(new_node);
+                return new_node;
+            }
+
+            // TODO: add erase method
+            resources_hash_node *find(const std::string &key) {
+                auto hash = ELFhash(key.c_str());
+                auto index = index_type(hash % m_nodes.size());
+                while (m_nodes[index] != nullptr)
+                {
+                    auto node = m_nodes[index];
+                    if (hash == node->hash && std::strcmp(key.c_str(), node->key.c_str()) == 0) {
+                        return m_nodes[index];
+                    }
+                    auto next_index = m_nodes[index]->next;
+                    if (next_index < 0) break;
+                    index = index_type(next_index);
+                }
+                return nullptr;
+            }
+
+            size_t size() const {
+                return m_size;
+            }
+
+        private:
+            void insert(resources_hash_node *node) {
+                if (node == nullptr) return;
+
+                if (m_size >= m_nodes.size() / 2) {
+                    rehash(m_nodes.size() * 2);
+                }
+
+                ++m_size;
+                auto hash = node->hash;
+                auto index = index_type(hash % m_nodes.size());
+                auto first_aid_index = index;
+                if (m_nodes[index] == nullptr) {
+                    m_nodes[index] = node;
+                    return;
+                }
+                do {
+                    auto anchor = m_nodes[index];
+                    auto anchor_hash = anchor->hash;
+                    auto anchor_index = index_type(anchor_hash % m_nodes.size());
+                    // if (anchor_hash == hash && anchor->key == key) {}
+                    if (anchor_index == first_aid_index) {
+                        auto next_index = anchor->next;
+                        if (next_index < 0) {
+                            // a.1 insert at empty slot
+                            node->next = -1;
+                            anchor->next = insert_at_next_ready(node);
+                            break;  // break when m_nodes[index]->next < 0
+                        }
+                        index = index_type(next_index);
+                    } else {
+                        // b.1 find pre-next value
+                        auto pre_next_index = index_type(anchor_hash % m_nodes.size());
+                        if (m_nodes[pre_next_index] == nullptr) {
+                            m_nodes[pre_next_index] = anchor;
+                            m_nodes[index] = node;
+                            break;
+                        }
+                        while (m_nodes[pre_next_index]->next >= 0 &&
+                                m_nodes[pre_next_index]->next != index)
+                        {
+                            pre_next_index = index_type(m_nodes[pre_next_index]->next);
+                        }
+                        m_nodes[pre_next_index]->next = update_conflict_node(anchor);
+                        m_nodes[index] = node;
+                        break;  // break when hash conflict
+                    }
+                } while (m_nodes[index] != nullptr);    ///< always true
+            }
+
+            index_type update_conflict_node(resources_hash_node *node) {
+                auto hash = node->hash;
+                auto index = index_type(hash % m_nodes.size());
+                if (m_nodes[index] == nullptr) {
+                    m_nodes[index] = node;
+                    return index;
+                }
+                return insert_at_next_ready(node);
+            }
+
+            index_type insert_at_next_ready(resources_hash_node *node) {
+                while (m_nodes[m_next_ready] != nullptr) m_next_ready = (m_next_ready + 1) % m_nodes.size();
+                auto index = index_type(m_next_ready);
+                m_next_ready = (m_next_ready + 1) % m_nodes.size();
+                m_nodes[index] = node;
+                return index;
+            }
+
+            void rehash(size_t size) {
+                if (size < m_nodes.size()) size = m_nodes.size();
+                resources_has_table temp((unsigned int)(size));
+                for (auto &node : m_nodes) {
+                    if (node == nullptr) continue;
+                    node->next = -1;
+                    temp.insert(node);
+                }
+                m_nodes = temp.m_nodes;
+                m_next_ready = temp.m_next_ready;
+                m_size = temp.m_size;
+                temp.m_nodes.clear();
+                temp.m_next_ready = 0;
+                temp.m_size = 0;
+            }
+
+        private:
+            std::vector<resources_hash_node*> m_nodes;
+            size_t m_next_ready = 0;
+            size_t m_size = 0;
         };
 
         class code_block {
@@ -183,25 +343,25 @@ namespace orz {
                     out_source << std::endl;
                 }
 
-                out_source << "const struct orz_resources orz_resources_get(const char *url)" << std::endl;
+                out_source << "const struct orz_resources orz_resources_get(const char *key)" << std::endl;
                 out_source << "{" << std::endl;
                 out_source << "    struct orz_resources resources;" << std::endl;
                 out_source << "    resources.data = NULL;" << std::endl;
                 out_source << "    resources.size = 0;" << std::endl;
-                out_source << "    if (!url)" << std::endl;
+                out_source << "    if (!key)" << std::endl;
                 out_source << "    {" << std::endl;
                 out_source << "        return resources;" << std::endl;
                 out_source << "    }" << std::endl;
-                out_source << "    if (url[0] == '@')" << std::endl;
+                out_source << "    if (key[0] == '@')" << std::endl;
                 out_source << "    {" << std::endl;
-                out_source << "        url++;" << std::endl;
+                out_source << "        key++;" << std::endl;
                 out_source << "    }" << std::endl;
 
                 // 1.2 write usage
                 for (size_t i = 0; i < in_files.size(); ++i) {
                     auto &res = in_resources[i];
                     if (i) out_source << "    else" << std::endl;
-                    code_block::usage(out_source, res.url, "url", "resources", "    ");
+                    code_block::usage(out_source, res.url, "key", "resources", "    ");
                     // out_source << std::endl;
                 }
 
--- test/resources.cpp
+++ test/resources.cpp
@@ -1,4 +1,86 @@
 //
-// Created by seetadev on 2018/9/25.
+// Created by kier on 2018/9/25.
 //
+
+#include <orz/tools/resources.h>
+#include <sstream>
+#include <fstream>
+#include <orz/utils/random.h>
+
+static std::string in_source =
+        "/file1:file1\n"
+        "/file2:file2\n";
+
+static std::string in_binary =
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        "abcdefghigklmn"
+        ;
+
+std::string random_string(size_t length) {
+    const char letter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const size_t letter_size = sizeof(letter) / sizeof(letter[0]) - 1;
+    std::string rstr(length, '\0');
+    for (auto &ch : rstr) {
+        ch = letter[orz::random.next(0, letter_size - 1)];
+    }
+    return std::move(rstr);
+}
+
+std::string random_string() {
+    return random_string(size_t(orz::random.next(1, 16)));
+}
+
+int main() {
+    std::istringstream iss_source(in_source);
+    std::istringstream iss_binary(in_binary);
+
+    std::ifstream resources_file("in.src.txt");
+
+    orz::resources::compiler compiler;
+    std::ofstream header("orz_resources.h");
+    std::ofstream source("orz_resources.c");
+    compiler.compile(resources_file, header, source);
+
+    // test hash table
+    int M = 1000;
+    int N = 1000;
+    orz::random.seed(4481);
+    int count = 0;
+    for (int i = 0; i < M; ++i) {
+        orz::resources::resources_has_table table;
+        std::vector<std::string> rstr(N);
+        for (int j = 0; j < N; ++j) {
+            rstr[j] = random_string();
+        }
+        for (int j = 0; j < N; ++j) {
+            table.insert(rstr[j]);
+        }
+        for (int j = 0; j < N; ++j) {
+            auto &str = rstr[j];
+            auto res = table.find(str);
+            if (res) {
+                // std::cout << str << " vs. " << res->key << std::endl;
+                if (str != res->key) {
+                    std::cout << "error type 0 in i = " << i << ", j = " << j << std::endl;
+                    ++count;
+                }
+            } else {
+                // std::cout << str << " vs. " << std::endl;
+                std::cout << "error type 1 in i = " << i << ", j = " << j << std::endl;
+                ++count;
+            }
+        }
+    }
+    std::cout << "failed rate: " << float(count) / (M * N) << std::endl;
+
+    return 0;
+}
 
