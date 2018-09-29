6cccef3b7b1c1e75d8dde11edd713095c960cded
kaizhou.li <kaizhou.li@seetatech.com>
2018-09-29 15:21:54 +0800

    fix ORZResources name definations

--- lokatme/ORZResources.cmake
+++ lokatme/ORZResources.cmake
@@ -10,31 +10,35 @@ else()
 endif()
 
 function(add_orz_resources var_output_dir var_headers var_sources)
+    # set names
+    set(OUTPUT_SUB_DIR .resources)
+    set(OUTPUT_FILENAME orz_resources)
     # checking orc file
     file(GLOB FOUND_ORZ_RESOURCES_ORC
-            "${var_output_dir}/orz_resources.orc")
+            "${var_output_dir}/${OUTPUT_FILENAME}.orc")
 
     list(LENGTH FOUND_ORZ_RESOURCES_ORC FOUND_ORZ_RESOURCES_ORC_SIZE)
-
+    # outputing orc file
     if (${FOUND_ORZ_RESOURCES_ORC_SIZE} LESS 1)
-        message(STATUS "Generating empty orc file: ${var_output_dir}/orz_resources.orc")
-        file(WRITE "${var_output_dir}/orz_resources.orc" "# Add every line to the format /url:path.\n")
+        message(STATUS "Generating empty orc file: ${var_output_dir}/${OUTPUT_FILENAME}.orc")
+        file(WRITE "${var_output_dir}/${OUTPUT_FILENAME}.orc" "# Add every line to the format /url:path.\n")
     endif()
 
     set(${var_headers})
     set(${var_sources})
 
-    list(APPEND ${var_headers} "${var_output_dir}/resources/orz_resources.h")
-    list(APPEND ${var_sources} "${var_output_dir}/resources/orz_resources.c")
+    list(APPEND ${var_headers} "${var_output_dir}/${OUTPUT_SUB_DIR}/${OUTPUT_FILENAME}.h")
+    list(APPEND ${var_sources} "${var_output_dir}/${OUTPUT_SUB_DIR}/${OUTPUT_FILENAME}.c")
 
-        message(STATUS "${var_output_dir}/resources")
+    include_directories("${var_output_dir}/${OUTPUT_SUB_DIR}")
 
     # compiling orc file
     add_custom_command(
-            OUTPUT "${var_output_dir}/resources/orz_resources.c"
-            COMMAND orz_resources "${var_output_dir}/orz_resources.orc" "--out_dir=${var_output_dir}/resources"
-            DEPENDS "${var_output_dir}/orz_resources.orc"
-            COMMENT "Compiling resources file ${var_output_dir}/orz_resources.orc"
+            OUTPUT "${var_output_dir}/${OUTPUT_SUB_DIR}/${OUTPUT_FILENAME}.c"
+            COMMAND orz_resources "${var_output_dir}/${OUTPUT_FILENAME}.orc" "--out_dir=${var_output_dir}/${OUTPUT_SUB_DIR}"
+            # "--filename=${OUTPUT_FILENAME}"
+            DEPENDS "${var_output_dir}/${OUTPUT_FILENAME}.orc"
+            COMMENT "Compiling resources file ${var_output_dir}/${OUTPUT_FILENAME}.orc"
             VERBATIM )
 
     set_source_files_properties(${${var_headers}} ${${var_sources}} PROPERTIES GENERATED TRUE)
